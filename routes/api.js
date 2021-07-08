fa9e8c1d011e5d1c36de382193247a5955739ca2
Tyler Davis <tdnc85@yahoo.com>
2021-07-07 23:25:27 -0400

    formatted routes files

--- routes/api.js
+++ routes/api.js
@@ -1,29 +1,30 @@
 const router = require("express").Router();
-const DB = require("../db/db")
+const DB = require("../db/db");
 
 router.get("/api/notes", (req, res) => {
-    DB.getNotes().then(response => {
-        res.json(response)
-    }).catch(err => {
-        res.json(err)
-    }) 
-})
+  DB.getNotes()
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 router.post("/api/notes", (req, res) => {
-    DB.addNotes(req.body)
+  DB.addNotes(req.body)
     .then((userNote) => res.json(userNote))
-    .catch(err => {
-        res.json(err)
-    }) 
-})
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
-router.delete("/api/notes/:id", (req,res) => {
-    DB.deleteNote(req.params.id)
+router.delete("/api/notes/:id", (req, res) => {
+  DB.deleteNote(req.params.id)
     .then(() => res.json({ ok: true }))
-    .catch(err => {
-        res.json(err)
-    }) 
-})
-
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
--- routes/view.js
+++ routes/view.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 const path = require("path");
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
-})
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"))
-})
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
 module.exports = router;
